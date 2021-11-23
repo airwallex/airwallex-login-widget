36b0dee2b1f31a1c20a94df7482d86499baba24e
Josh Fu <josh.fu@airwallex.com>
2021-11-23 17:06:58 +0800

    feat: upgrade commit lint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,5 @@
+/** @type { import('@commitlint/types').UserConfig } */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 98],
-  },
+  ignores: [(commit) => commit.includes('chore(release)')],
 };
